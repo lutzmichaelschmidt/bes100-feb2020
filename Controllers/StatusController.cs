6c49bf30b696e80359199f2d75b2300d96670db5
lutzmichaelschmidt <61434839+lutzmichaelschmidt@users.noreply.github.com>
2020-02-24 21:00:07 +0000

    Done with Monday

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : Controller
+    {
+        [HttpGet("status")]
+        public ActionResult<StatusResponse> GetStatus()
+        {
+            var response = new StatusResponse
+            {
+                Status = "OK",
+                CreatedAt = DateTime.Now
+            };
+            return Ok(response);
+        }
+    }
+
+    public class StatusResponse
+    {
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
