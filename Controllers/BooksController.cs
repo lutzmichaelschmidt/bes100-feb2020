c4c2128f282ab4f2df57bcae7f12550d6549f9e0
lutzmichaelschmidt <61434839+lutzmichaelschmidt@users.noreply.github.com>
2020-02-26 18:05:45 +0000

    Wednesday PM

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using LibraryApi.Domain;
 using LibraryApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,19 +18,88 @@ namespace LibraryApi.Controllers
             Context = context;
         }
 
+        [HttpPost("books")]
+        public async Task<ActionResult<GetABookResponse>> AddABook([FromBody] PostBookRequest bookToAdd)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var book = new Book
+            {
+                Title = bookToAdd.Title,
+                Author = bookToAdd.Author,
+                Genre = bookToAdd.Genre,
+                InInventory = true,
+                NumberOfPages = bookToAdd.NumberOfPages
+            };
+
+            Context.Books.Add(book);
+            await Context.SaveChangesAsync();
+
+            var response = new GetABookResponse
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Author = book.Author,
+                Genre = book.Genre,
+                NumberOfPages = book.NumberOfPages
+            };
+
+            return CreatedAtRoute("books#getabook", new { id = book.Id }, response);
+
+        }
+
+        [HttpGet("books/{id:int}", Name ="books#getabook")]
+        public async Task<ActionResult<GetABookResponse>> GetABook(int id)
+        {
+           
+            var response = await GetBooksInInventory()
+                .Where(b => b.Id == id)
+                .Select(b => new GetABookResponse
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Genre = b.Author,
+                    NumberOfPages= b.NumberOfPages
+                }).SingleOrDefaultAsync();
+
+            if(response == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+
         [HttpGet("books")]
-        public ActionResult GetAllBooks()
+        public async Task<ActionResult<GetBooksResponse>> GetAllBooks([FromQuery]string genre="all")
         {
             var response = new GetBooksResponse();
-            response.Data = Context.Books
-                .Where(b => b.InInventory)
+
+            var data = GetBooksInInventory();
+            
+            if(genre != "all")
+            {
+                data = data.Where(b => b.Genre == genre);
+            }
+
+            response.Data = await data
                 .Select(b => new BookSummaryItem
                 {
                     Id = b.Id,
                     Title = b.Title,
                     Author = b.Author
-                }).ToList();
+                }).ToListAsync();
+            response.Genre = genre;
             return Ok(response);
+        }
+
+        private IQueryable<Book> GetBooksInInventory()
+        {
+            return Context.Books.Where(b => b.InInventory);
         }
     }
 }
